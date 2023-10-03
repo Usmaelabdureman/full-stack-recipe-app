8411b925cbaa30b7c9fb0c9a42ec1e950aeec290
Usmaelabdureman <uabdureman@gmail.com>
2023-10-03 18:36:33 +0300

    correct import

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const MONGO_URI=process.env.MONGO_URI;
 
-const typeDefs = require("./graphql/typedefs");
+const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
  const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
