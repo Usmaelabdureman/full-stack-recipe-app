8d3876c7981c6f9f0b82006d0f7e5d4cfc077e5b
Usmaelabdureman <uabdureman@gmail.com>
2023-10-03 18:30:41 +0300

    correct  name error

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const MONGO_URI=process.env.MONGO_URI;
 
 const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
-
+ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -15,10 +15,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGO_URI, { useNewUrlParser: true })
     .then(() => {
         console.log("MongoDB Connected");
-        return server.listen({ port: 5000 });
+        return server.listen({ port: PORT });
     })
     .then((res) => {
         console.log(`Server running at ${res.url}`);
+
     });
 
 
