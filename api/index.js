1e6935a87e1b0919fe0d0a085191f635e039dd5c
Usmaelabdureman <uabdureman@gmail.com>
2023-10-03 11:51:15 +0300

    update mongo uri

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const {ApolloServer} =require("apollo-server");
 const mongoose = require("mongoose");
 
-// const MONGO_URI=process.env.MONGO_URI;
-MONGO_URI='mongodb+srv://usmael:tododb@todo-db.2epuypc.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URI=process.env.MONGO_URI;
 
 const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
