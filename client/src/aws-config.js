628ad2458fac0185cd44f7eb70be8829330bc7c5
Usmaelabdureman <uabdureman@gmail.com>
2023-10-20 20:02:00 +0300

    added chatbot

--- client/src/aws-config.js
+++ client/src/aws-config.js
@@ -7,8 +7,8 @@ import AWS from 'aws-sdk';
 
 // Configure AWS services with your credentials and region
 AWS.config.update({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  accessKeyId: 'AKIA3KL5YXS5X4X2DJ6D',
+  secretAccessKey: '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',
   region: 'eu-north-1',
 });
 
